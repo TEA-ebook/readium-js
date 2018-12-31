2cb84ba9c596f30aca9486a80858a5b86d3873c7
Johan Poirier <johan.poirier@gmail.com>
2018-12-31 09:18:47 +0100

    Fix use of Array.includes not compatible with IE

--- js/epub-fetch/lcp_handler.js
+++ js/epub-fetch/lcp_handler.js
@@ -24,6 +24,7 @@ define(['forge', 'promise', 'pako'], function (forge, es6Promise, pako) {
     var READ_AS_BINARY_STRING_AVAILABLE = typeof FileReader.prototype.readAsBinaryString === 'function';
 
     es6Promise.polyfill();
+    objectPolyfill();
 
     var LcpHandler = function (encryptionData, onError) {
 
@@ -80,7 +81,7 @@ define(['forge', 'promise', 'pako'], function (forge, es6Promise, pako) {
                 });
 
                 // encryption profile
-                if (!lcpProfiles.includes(license.encryption.profile)) {
+                if (lcpProfiles.indexOf(license.encryption.profile) === -1) {
                     errors.push("Unknown encryption profile '" + license.encryption.profile + "'");
                 }
 
@@ -345,3 +346,35 @@ define(['forge', 'promise', 'pako'], function (forge, es6Promise, pako) {
 
     return LcpHandler;
 });
+
+function objectPolyfill() {
+    if (typeof Object.assign != 'function') {
+        // Must be writable: true, enumerable: false, configurable: true
+        Object.defineProperty(Object, "assign", {
+            value: function assign(target, varArgs) { // .length of function is 2
+                'use strict';
+                if (target == null) { // TypeError if undefined or null
+                    throw new TypeError('Cannot convert undefined or null to object');
+                }
+
+                var to = Object(target);
+
+                for (var index = 1; index < arguments.length; index++) {
+                    var nextSource = arguments[index];
+
+                    if (nextSource != null) { // Skip over if undefined or null
+                        for (var nextKey in nextSource) {
+                            // Avoid bugs when hasOwnProperty is shadowed
+                            if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+                                to[nextKey] = nextSource[nextKey];
+                            }
+                        }
+                    }
+                }
+                return to;
+            },
+            writable: true,
+            configurable: true
+        });
+    }
+}

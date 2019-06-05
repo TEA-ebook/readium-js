d70dd203ae3302fdfa182d731e3dbbc3a9d8b9e6
Johan Poirier <johan.poirier@gmail.com>
2019-06-05 14:00:26 +0200

    Add xml extension for spine xhtml mime type

--- js/epub-fetch/discover_content_type.js
+++ js/epub-fetch/discover_content_type.js
@@ -1,14 +1,14 @@
 //  Copyright (c) 2014 Readium Foundation and/or its licensees. All rights reserved.
-//  
-//  Redistribution and use in source and binary forms, with or without modification, 
+//
+//  Redistribution and use in source and binary forms, with or without modification,
 //  are permitted provided that the following conditions are met:
-//  1. Redistributions of source code must retain the above copyright notice, this 
+//  1. Redistributions of source code must retain the above copyright notice, this
 //  list of conditions and the following disclaimer.
-//  2. Redistributions in binary form must reproduce the above copyright notice, 
-//  this list of conditions and the following disclaimer in the documentation and/or 
+//  2. Redistributions in binary form must reproduce the above copyright notice,
+//  this list of conditions and the following disclaimer in the documentation and/or
 //  other materials provided with the distribution.
-//  3. Neither the name of the organization nor the names of its contributors may be 
-//  used to endorse or promote products derived from this software without specific 
+//  3. Neither the name of the organization nor the names of its contributors may be
+//  used to endorse or promote products derived from this software without specific
 //  prior written permission.
 
 define(['jquery', 'URIjs'], function ($, URI) {
@@ -31,7 +31,8 @@ define(['jquery', 'URIjs'], function ($, URI) {
             opf: 'application/oebps-package+xml',
             png: 'image/png',
             svg: 'image/svg+xml',
-            xhtml: 'application/xhtml+xml'
+            xhtml: 'application/xhtml+xml',
+            xml: 'application/xhtml+xml'
         };
 
         this.identifyContentTypeFromFileName = function(contentUrl) {
